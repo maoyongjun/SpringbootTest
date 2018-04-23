40001ebed5f6fc7488c2f62e6b8aa2c7e6ab93be
maoyongjun <H7108464@foxconn.com>
2018-04-23 13:55:01 +0800

    lock Test

---
+++ src/test/java/org/foxconn/springbootStart/TestLock.java
@@ -0,0 +1,10 @@
+package org.foxconn.springbootStart;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestLock {
+	public static void main(String[] args) {
+		Lock lock = new ReentrantLock();
+	}
+}
