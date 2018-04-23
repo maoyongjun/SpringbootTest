fa4b7b74e488f2f5d5b6c896dc3bf4bc0ff8ec1a
mao_yongjun <mao_yongjun@DESKTOP-6AICMB0>
2018-04-23 20:16:40 +0800

    test

--- src/main/java/org/springframework/transaction/support/Test.java
+++ src/main/java/org/springframework/transaction/support/Test.java
@@ -1,5 +1,12 @@
 package org.springframework.transaction.support;
 
+import java.util.concurrent.locks.Lock;
+
 public class Test {
-	
+		public static void main(String[] args) {
+			System.out.println("main".substring(1,3));
+			
+			Lock
+			
+		}
 }
