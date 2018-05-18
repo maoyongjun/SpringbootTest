34db3bb9c70cee8110d496ab9cbf23a77c594e24
maoyongjun <H7108464@foxconn.com>
2018-05-18 17:54:32 +0800

    solve confict

--- src/main/java/org/foxconn/springbootStart/Application.java
+++ src/main/java/org/foxconn/springbootStart/Application.java
@@ -3,10 +3,10 @@ package org.foxconn.springbootStart;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.apache.solr.client.solrj.SolrClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.context.WebApplicationContext;
@@ -15,44 +15,34 @@ import org.springframework.web.context.WebApplicationContext;
 @MapperScan("org.foxconn.springbootStart.dao")
 public class Application extends SpringBootServletInitializer {
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		// TODO Auto-generated method stub
-		super.onStartup(servletContext);
-	}
-
-	@Override
-	public WebApplicationContext createRootApplicationContext(ServletContext servletContext) {
-		// TODO Auto-generated method stub
-		return super.createRootApplicationContext(servletContext);
-	}
-
-	@Override
-	protected SpringApplicationBuilder createSpringApplicationBuilder() {
-		// TODO Auto-generated method stub
-		return super.createSpringApplicationBuilder();
-	}
-
-	@Override
-	protected WebApplicationContext run(SpringApplication application) {
-		// TODO Auto-generated method stub
-		return super.run(application);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return super.configure(builder);
-	}
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
+//	@Override
+//	public void onStartup(ServletContext servletContext) throws ServletException {
+//		// TODO Auto-generated method stub
+//		super.onStartup(servletContext);
+//	}
 //
-//    public static void main(String[] args) throws Exception {
-//        SpringApplication.run(Application.class, args);
-//    }
+//	@Override
+//	public WebApplicationContext createRootApplicationContext(ServletContext servletContext) {
+//		// TODO Auto-generated method stub
+//		return super.createRootApplicationContext(servletContext);
+//	}
+//
+//	@Override
+//	protected SpringApplicationBuilder createSpringApplicationBuilder() {
+//		// TODO Auto-generated method stub
+//		return super.createSpringApplicationBuilder();
+//	}
+//
+//	@Override
+//	protected WebApplicationContext run(SpringApplication application) {
+//		// TODO Auto-generated method stub
+//		return super.run(application);
+//	}
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
     
     
 
